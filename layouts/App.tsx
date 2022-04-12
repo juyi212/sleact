92fb896eb0b13897ff63346d9e5eb04cd7a98fe3
juyi212 <dea8307@naver.com>
2022-04-12 15:31:42 +0900

    router settings

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,8 +1,16 @@
+import LogIn from '@pages/LogIn';
+import SignUp from '@pages/SignUp'
 import React from 'react';
+import { Redirect, Switch, Route } from 'react-router-dom';
+
 
 const App = () => {
     return (
-        <div> ㅈㅅsdafㅈㅅ</div>
+        <Switch>
+            <Redirect exact path = '/' to='login' />
+            <Route path = '/login' component= {LogIn}></Route>
+            <Route path = '/signup' component= {SignUp}></Route>
+        </Switch>
     )
 }
 

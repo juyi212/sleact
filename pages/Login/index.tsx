90fd28b5eaa4aeb75fdfdea4f95d2357fb5d27b4
juyi212 <dea8307@naver.com>
2022-04-14 17:51:27 +0900

    swr settings

--- pages/Login/index.tsx
+++ pages/Login/index.tsx
@@ -2,10 +2,13 @@ import useInput from '@hooks/useInput';
 import { Button, Error, Form, Header, Input, Label, LinkContainer } from '@pages/SignUp/styles';
 import axios from 'axios';
 import React, { useCallback, useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import fetcher from '@utils/fetcher';
+import useSWR from 'swr';
 
 const LogIn = () => {
-//   const { data: userData, error, mutate } = useSWR('/api/users', fetcher);
+    const { data: userData, error, mutate }  = useSWR('http://localhost:3095/api/users', fetcher, {
+        dedupingInterval: 100000
+    });
   const [logInError, setLogInError] = useState(false);
   const [email, onChangeEmail] = useInput('');
   const [password, onChangePassword] = useInput('');
@@ -15,14 +18,14 @@ const LogIn = () => {
       setLogInError(false);
       axios
         .post(
-          '/api/users/login',
+          'http://localhost:3095/api/users/login',
           { email, password },
           {
             withCredentials: true,
           },
         )
         .then((res) => {
-          console.log(res)
+          mutate()
         })
         .catch((error) => {
           setLogInError(error.response?.data?.code === 401);
